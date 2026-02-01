26092acb9ed7fbe32bbc435b87b21f8104fd80b5
kiozeee <lm73112@ubt-uni.net>
2026-02-01 01:40:12 +0100

    DB local XAMPP

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $pass = "";
 $db   = "web_project";
-$port = 3307;
+$port = 3306;
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 if (!$conn) {
