1322641ea44236bb4f3b898bc323fb5ad9f1cfbf
emushi777 <el73809@ubt-uni.net>
2026-03-22 16:04:58 +0100

    session fix

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
 
 require_once "classes/Database.php";
 require_once "classes/Contact.php";
--- seed_books.php
+++ seed_books.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
 
 $conn = new mysqli("localhost", "root", "", "web_project");
 
